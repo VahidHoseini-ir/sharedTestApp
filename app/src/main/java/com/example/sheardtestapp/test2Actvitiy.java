3a9304e63d1cc18c30a2e0de89d4600124f89d5c
DCE-617-PC00 <test@test.ir>
2025-04-08 14:55:23 +0330

    test commit

---
+++ app/src/main/java/com/example/sheardtestapp/test2Actvitiy.java
@@ -0,0 +1,33 @@
+package com.example.sheardtestapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class test2Actvitiy extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView btn_test = findViewById(R.id.btn_test);
+
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_test.setText("test");
+
+
+
+    }
+}
